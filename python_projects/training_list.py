baddc1e76e7a66ceb393efc17f37cb49c22f3abf
Ana Beatriz Araújo de Sousa <ana.araujo.de.sousa@avanade.com>
2024-08-18 12:07:39 -0300

    tratamento para email valido

--- python_projects/training_list.py
+++ python_projects/training_list.py
@@ -1,4 +1,5 @@
-#colocar como saída a quantidade de pessoas que foram e o nome de cada uma
+import re
+
 
 def verificador_de_presenca(compareceu):
     return compareceu == 's'
@@ -16,14 +17,21 @@ class Pessoa():
     def __eq__(self, __other):
         return self.email == __other.email
 
+    def validar_email(self):
+        padrao_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return re.match(padrao_email, self.email) is not None
+
 
 pessoas = []
 qntd_alunos = int(input())
 for i in range(0, qntd_alunos):
     pessoa = input().split()
     pessoa = Pessoa(pessoa[0], pessoa[1], pessoa[2])
+    if not pessoa.validar_email():
+        print("Email inválido")
+        continue
     compareceu = input("Compareceu ao treinamento? s/n")
-    if verificador_de_presenca(compareceu):
+    if verificador_de_presenca(compareceu) and pessoa.validar_email():
         pessoas.append(pessoa)
 
 for pessoa in pessoas:
