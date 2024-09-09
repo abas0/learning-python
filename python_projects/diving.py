f432614743a843a1fbd8e3b6fd04b1bab8ea9f02
Ana Beatriz Araújo de Sousa <ana.araujo.de.sousa@avanade.com>
2024-09-09 20:52:19 -0300

    diving completo

--- python_projects/diving.py
+++ python_projects/diving.py
@@ -1,34 +1,20 @@
-notas = []
+organizar_tudo = []
+rankeando_notas = []
 organizar_todas_as_notas = []
-
-def imprimir_notas_de_um_nadador():
-    for j in range(0, 7):
-        nota = float(input("Insira uma nota: "))
-        interruptor = True
-        while interruptor:
-            if 0 <= nota <= 10:
-                notas.append(nota)
-                interruptor = not interruptor
-            else:
-                nota = float(input("Nota inválida. Insira novamente"))
+qtd_nadadores = int(input())
+nadadores = []
 
 
-def notas_para_calculo():
-    notas.sort()
-    notas.pop(0)
-    notas.pop(len(notas) - 1)
-    print(f"Notas dentro de organizar_notas(): {notas}")
-    return notas
-
-
-# def organizar_todas_as_notas():
-    # return []
+def ranking_participantes(lista_media_organizadas):
+    lista_media_organizadas.sort(key=lambda media_organizadas: media_organizadas['nota'], reverse=True)
+    return lista_media_organizadas
 
 
 class Nadador:
-    def __init__(self, nome: str, grau: float):
+    def __init__(self, nome: str, grau: float, notas: []):
         self.nome = nome
         self.grau = grau
+        self.notas = notas
 
     def validador_de_dificuldade(self):
         interruptor = True
@@ -39,6 +25,23 @@ class Nadador:
                 novo_grau = float(input("Grau inválido! Tente novamente"))
                 self.grau = novo_grau
 
+    def imprimir_notas_de_um_nadador(self):
+        for j in range(0, 7):
+            nota = float(input("Insira uma nota: "))
+            interruptor = True
+            while interruptor:
+                if 0 <= nota <= 10:
+                    self.notas.append(nota)
+                    interruptor = not interruptor
+                else:
+                    nota = float(input("Nota inválida. Insira novamente"))
+
+    def notas_para_calculo(self):
+        self.notas.sort()
+        self.notas.pop(0)
+        self.notas.pop(len(self.notas) - 1)
+        return self.notas
+
     def calcular_nota(self, notas_organizadas):
         somatorio = 0
         for j in range(0, len(notas_organizadas)):
@@ -47,15 +50,17 @@ class Nadador:
         return nota_final
 
 
-qtd_nadadores = int(input())
-nadadores = []
 for i in range(0, qtd_nadadores):
     dados_nadador = input("Digite o nome do nadador e o grau de dificuldade do seu nado").split()
-    nadador = Nadador(dados_nadador[0], float(dados_nadador[1]))
+    nadador = Nadador(dados_nadador[0], float(dados_nadador[1]), [])
     nadador.validador_de_dificuldade()
-    imprimir_notas_de_um_nadador()
-    print(nadador.nome, nadador.calcular_nota(notas))
-    organizar_todas_as_notas.append(nadador.calcular_nota(notas))
-    print(organizar_todas_as_notas)
-    # 2 parte: organizar as notas dos participantes em ordem decrescente
-    # 3 parte: organizar a lista de participantes em ordem decrescente pelos seus nomes
+    nadador.imprimir_notas_de_um_nadador()
+    nota_calculada = nadador.calcular_nota(nadador.notas_para_calculo())
+    print(nadador.nome, nota_calculada)
+    organizar_tudo.append({'nota': nota_calculada, 'nome': nadador.nome})
+    rankeando_notas = ranking_participantes(organizar_tudo)
+
+
+print("Ranking: ")
+for nadador in rankeando_notas:
+     print(f"{nadador['nome']}")
